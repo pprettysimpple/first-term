80c15f55af67f6a84b8e05990b9b9c0e85fa3c88
pprettysimpple <andrianov.kirill2010@yandex.ru>
2020-08-31 14:03:55 +0300

    minor fixes

--- vector/vector.h
+++ vector/vector.h
@@ -9,11 +9,9 @@
 #include <cassert>
 #include <utility>
 #include <stdexcept>
-#include "vector.h"
 
-template <typename T>
-struct vector
-{
+template<typename T>
+struct vector {
     using iterator = T*;
     using const_iterator = T const*;
 
@@ -54,35 +52,41 @@ struct vector
     const_iterator begin() const;           // O(1) nothrow
     const_iterator end() const;             // O(1) nothrow
 
-    iterator insert(const_iterator pos, T const&); // O(N) weak
+    iterator insert(const_iterator it, T const& value);         // O(N) weak
 
-    iterator erase(const_iterator pos);     // O(N) weak
+    iterator erase(const_iterator it);      // O(N) weak
 
-    iterator erase(const_iterator first, const_iterator last); // O(N) weak
+    iterator erase(const_iterator first, const_iterator last);      // O(N) weak
 
- private:
+private:
     size_t increase_capacity() const;
+
     void push_back_realloc(T const&);
+
     void new_buffer(size_t new_capacity);
 
- private:
+    void destroy_pref(T* pos, size_t count);
+
+    void copy_construct_all(T* dest, T const* source, size_t count);
+
+private:
     T* data_;
     size_t size_{};
     size_t capacity_{};
 };
 
 template<typename T>
-static void destroy_pref(T* pos, size_t count) {
+void vector<T>::destroy_pref(T* pos, size_t count) {
     while (count-- > 0) {
         (--pos)->~T();
     }
 }
 
 template<typename T>
-static void copy_construct_all(T* dest, T const *source, size_t count) {
+void vector<T>::copy_construct_all(T* dest, T const* source, size_t count) {
     for (size_t i = 0; i < count; i++) {
         try {
-            new (dest + i) T(source[i]);
+            new(dest + i) T(source[i]);
         } catch (...) {
             destroy_pref(dest + i, i);
             throw;
@@ -95,13 +99,11 @@ vector<T>::vector() : data_(nullptr), size_(0), capacity_(0) {}
 
 template<typename T>
 vector<T>::vector(vector<T> const& other) : data_(nullptr), size_(0), capacity_(0) {
-    if (other.size_) {
-        data_ = reinterpret_cast<T*>(operator new [](other.size_ * sizeof(T)));
-    }
+    new_buffer(other.size());
     try {
         copy_construct_all(data_, other.data_, other.size_);
     } catch (...) {
-        operator delete [] (data_);
+        operator delete(data_);
         throw;
     }
     size_ = other.size_;
@@ -110,10 +112,8 @@ vector<T>::vector(vector<T> const& other) : data_(nullptr), size_(0), capacity_(
 
 template<typename T>
 vector<T>::~vector<T>() {
-    if (data_ != nullptr) {
-        destroy_pref(data_ + size_, size_);
-        operator delete[] (data_);
-    }
+    destroy_pref(data_ + size_, size_);
+    operator delete(data_);
 }
 
 template<typename T>
@@ -121,16 +121,16 @@ vector<T>& vector<T>::operator=(vector<T> const& other) {
     if (this == &other) {
         return *this;
     }
-    vector<T> copy(other);
-    vector<T>::swap(copy);
+    vector<T>(other).swap(*this);
     return *this;
 }
 
 template<typename T>
-void vector<T>::swap(vector<T> &other) {
-    std::swap(data_, other.data_);
-    std::swap(size_, other.size_);
-    std::swap(capacity_, other.capacity_);
+void vector<T>::swap(vector<T>& other) {
+    using std::swap;
+    swap(data_, other.data_);
+    swap(size_, other.size_);
+    swap(capacity_, other.capacity_);
 }
 
 template<typename T>
@@ -232,19 +232,19 @@ template<typename T>
 void vector<T>::new_buffer(size_t new_capacity) {
     T* old_buff = data_;
     if (new_capacity) {
-        data_ = reinterpret_cast<T*>(operator new[] (new_capacity * sizeof(T)));
+        data_ = static_cast<T*>(operator new(new_capacity * sizeof(T)));
     } else {
         data_ = nullptr;
     }
     try {
         copy_construct_all(data_, old_buff, size_);
     } catch (...) {
-        operator delete [] (data_);
+        operator delete(data_);
         data_ = old_buff;
         throw;
     }
     destroy_pref(old_buff + size_, size_);
-    operator delete [] (old_buff);
+    operator delete(old_buff);
     capacity_ = new_capacity;
 }
 
@@ -254,10 +254,10 @@ size_t vector<T>::increase_capacity() const {
 }
 
 template<typename T>
-void vector<T>::push_back_realloc(const T &value) {
+void vector<T>::push_back_realloc(const T& value) {
     T tmp(value);
     new_buffer(increase_capacity());
-    new (data_ + size_) T(tmp);
+    new(data_ + size_) T(tmp);
     size_++;
 }
 
@@ -268,16 +268,16 @@ void vector<T>::pop_back() {
 }
 
 template<typename T>
-void vector<T>::push_back(const T &value) {
+void vector<T>::push_back(const T& value) {
     if (size_ == capacity_) {
         push_back_realloc(value);
     } else {
-        new (data_ + size_++) T(value);
+        new(data_ + size_++) T(value);
     }
 }
 
 template<typename T>
-typename vector<T>::iterator vector<T>::insert(vector<T>::const_iterator it, T const &value) {
+typename vector<T>::iterator vector<T>::insert(vector<T>::const_iterator it, T const& value) {
     assert(it >= data_);
     ptrdiff_t insert_pos = it - data_;
     ptrdiff_t current_pos = size_;
@@ -301,7 +301,8 @@ typename vector<T>::iterator vector<T>::erase(vector<T>::const_iterator first, v
     ptrdiff_t len = last - first;
     ptrdiff_t shift = res;
     while (shift + len < size_) {
-        data_[shift] = data_[shift + len];
+        using std::swap;
+        swap(data_[shift], data_[shift + len]);
         shift++;
     }
     while (len-- > 0) {
